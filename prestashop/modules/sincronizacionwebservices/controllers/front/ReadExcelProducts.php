057e887a08ec537c3afb11a5a10fff8d36c6ddc5
chxt0 <chxto1906@gmail.com>
2019-12-02 13:07:49 -0500

    nuevos cambios para sincronizar precios y stock de productos

--- prestashop/modules/sincronizacionwebservices/controllers/front/ReadExcelProducts.php
+++ prestashop/modules/sincronizacionwebservices/controllers/front/ReadExcelProducts.php
@@ -30,16 +30,19 @@ class sincronizacionwebservicesReadExcelProductsModuleFrontController extends Mo
 
         $productsPrices = $gestionarController->_get_ws_products_catalogo();
         $productsPrices = !empty($productsPrices->ARTICULOS) ? $productsPrices->ARTICULOS : [];
-        if ($parte == 1)
-            $updateActiveResult = $gestionarController->desactivate_all_products_for_shop($id_shop);
+
+        $productsStocks = $gestionarController->_get_ws_products();
+        $productsStocks = !empty($productsStocks->STOCK) ? $productsStocks->STOCK : [];
+        /*if ($parte == 1)
+            $updateActiveResult = $gestionarController->desactivate_all_products_for_shop($id_shop);*/
         foreach ($getSheet as $value) {
             //if ($i > 2) {
                 
                     $reference = $value["0"];
                     $activar = trim(strtolower($value["2"]));
                     $active = $activar == "x" ? 1 : 0;
-                    if ($active == 1)
-                        $this->proccess($id_shop,$reference,$active,$productsPrices);
+                    //if ($active == 1)
+                        $this->proccess($id_shop,$reference,$active,$productsPrices,$productsStocks);
                     //echo "<br> REFERENCE: $reference, ACTIVAR: $active <br>";
             //}
             //$i++;
@@ -51,53 +54,48 @@ class sincronizacionwebservicesReadExcelProductsModuleFrontController extends Mo
 
 
 
-    public function proccess($id_shop,$reference,$active,$productsPrices) {
+    public function proccess($id_shop,$reference,$active,$productsPrices,$productsStocks) {
         $gestionarController = new sincronizacionwebservicesGestionarProductosModuleFrontController();
         $facturaSAP = new sincronizacionwebservicesFacturaSAPModuleFrontController();
         $cod_almacen = $gestionarController->get_code_by_id($id_shop);
         $id_product = $gestionarController->search_product_by_reference($reference);
+        $stock = 0;
+        $active = 0;
         if ($cod_almacen && $id_product) {
-            $articulo = $facturaSAP->recuperaArticulo($cod_almacen,$reference);
-            if (is_array($articulo)) {
-                if ($articulo[0] == "0"){
-                    $stock = (int)$articulo[15];
-                    $stock = $stock - BUFFER_STOCK;
-                    echo "<br>STOCK seteado para product reference: $reference Es: $stock<br>";
-                    if ($stock <= 0) {
-                        $active = 0;
-			echo "<br>### El active se puso en 0 ###<br>";
-                    } else {
-                        $productPrice = $this->searchArray($reference,$productsPrices);
-			//echo "<br>ProductPrice ENCONTRADO: $productPrice<br>";
-                        if ($productPrice) {
-                            $marca = $productPrice->Marca;
-                            $precio = $productPrice->Precio;
-                            $added = $gestionarController->create_update_product($id_product,$reference,$marca,$precio);
-                            if ($added) {
-                                $active = 1;
-                                $message = "<br> Guardado producto reference: $reference correctamente <br>";
-                                echo $message;
-                            } else {
-                                $active = 0;
-                                $message = "<br>No pudo ser actualizado el producto reference $reference con el precio: $precio <br>";
-                                echo $message;
-                            }
-                        } else {
-                            $active = 0;
-                        }
-                    }
+            $articulo = $this->searchArrayStock($reference,$cod_almacen,$productsStocks);
+            $productPrice = $this->searchArray($reference,$productsPrices);
+            if ($articulo && $productPrice) {
+                $stock = (int)$articulo->MARLIB;
+                $stock = $stock - BUFFER_STOCK;
+                echo "<br>STOCK seteado para product reference: $reference Es: $stock<br>";
+		        echo "<br>### El active se puso en $active ###<br>";
+                $marca = $productPrice->Marca;
+                $precio = $productPrice->Precio;
+                $added = $gestionarController->create_update_product($id_product,$reference,$marca,$precio);
+                if ($added) {
+                    $active = $stock <= 0 ? 0 : 1;
+                    $message = "<br> Guardado producto reference: $reference correctamente <br>";
+                    echo $message;
                 } else {
                     $active = 0;
+                    $message = "<br>No pudo ser actualizado el producto reference $reference con el precio: $precio <br>";
+                    echo $message;
                 }
-            } else {
-                $active = 0;
             }
-            if ($active == 1)
-                StockAvailable::setQuantity($id_product, 0, (int) $stock, $id_shop);
-            $activateProducts = $this->activateProducts($id_product, $active, $id_shop);
         }
+        StockAvailable::setQuantity($id_product, 0, (int) $stock, $id_shop);
+        $activateProducts = $this->activateProducts($id_product, $active, $id_shop);
     }
 
+
+    public function searchArrayStock($id, $cod_almacen, $array) {
+       foreach ($array as $key) {
+           if (($key->MARMAT == $id) && ($key->MARALM == $cod_almacen)) {
+               return $key;
+           }
+       }
+       return null;
+    }
 
     public function searchArray($id, $array) {
        foreach ($array as $key) {
