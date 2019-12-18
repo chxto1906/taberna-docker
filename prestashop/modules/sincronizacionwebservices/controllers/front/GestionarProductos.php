5378191ca1b66f1104d4b72379f6cbd3a44d4dd4
chxt0 <chxto1906@gmail.com>
2019-12-18 09:53:50 -0500

    print product a actualizar en ReadExcelProducts

--- prestashop/modules/sincronizacionwebservices/controllers/front/GestionarProductos.php
+++ prestashop/modules/sincronizacionwebservices/controllers/front/GestionarProductos.php
@@ -345,6 +345,9 @@
             }
 
             try {
+                echo "<hr>";
+                var_dump($product);
+                echo "<hr>";
                 if ($product->save()) {
                     echo "<b>Pasado el save()</b>";
                     //$product->associateTo($shops);
